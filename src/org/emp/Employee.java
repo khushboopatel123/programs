64924233c70a17d7d099051d470e954a0897969c
khushboopatel <khushboopatelgurjar@gmail.com>
2020-01-20 13:48:34 +0530

    code added

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,20 @@
+package org.emp;
+
+import java.util.Scanner;
+
+public class Employee {
+	
+	public static void main(String[] args) {
+		 Scanner s=new Scanner(System.in);
+		System.out.println("enter the name");
+		System.out.println("empid");
+		
+		
+		System.out.println("name");
+		System.out.println("phone");
+		System.out.println("salary");
+		System.out.println("gender");
+		System.out.println("city");
+	}
+
+}
